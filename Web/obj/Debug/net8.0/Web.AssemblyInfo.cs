c7c4763c4c777571a7aa0c8a24a4940967ac83f7
Krebkel <medvedev.andrew@mail.com>
2024-01-17 02:52:35 +0700

    Added DbContext with entity configurations; added migrations

--- Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+201fcca058ff3e6fc92aa483407832505d648c90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e692eed5f3368691e44dafecdedc6c07393017bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
