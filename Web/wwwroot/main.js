05070413c0b57fe878a974ab24178d3fc80ec08d
Krebkel <medvedev.andrew@mail.com>
2024-01-20 10:14:02 +0700

    conceptual 404 coding

--- Web/wwwroot/main.js
+++ Web/wwwroot/main.js
@@ -1,4 +1,7 @@
-import { fetchInvoices } from './lib/UtilsModule.js';
+import { addInvoice, addPosition, addPositionRowToUpdateForm, addNewInvoice } from './lib/AddModule.js';
+import { deletePosition, removePositionRowToUpdateForm } from './lib/DeleteModule.js';
+import { validateNumberInput, createStyledButton, calculateTotalAmount, fetchInvoices, hideUpdateInvoiceForm, hideAddInvoiceForm} from './lib/UtilsModule.js';
+import { showUpdateInvoiceForm, getPositionsByInvoiceId, findInvoiceById, showPositions, showAddInvoiceForm } from './lib/GetModule.js';
 
 
 fetchInvoices();
